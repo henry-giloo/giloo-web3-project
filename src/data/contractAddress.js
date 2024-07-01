261a045382295f7052d8215f325e29d9a8cd5bfd
henry.hsu <henry.hsu@giloo.ist>
2024-07-01 12:06:52 +0800

    demo version

---
+++ src/data/contractAddress.js
@@ -0,0 +1,12 @@
+// src/data/nftContracts.js
+const nftContracts = [
+    '0x8e4f217887FC207C32d2055A490b2e77f28d51f2',
+    '0xB23D78c22d0d28Decb681f7000B0AE90ED44bBc7',
+  ];
+
+const tokenAddress = "0x0933f937d4b5c9ed6d7B39b4C187AB6d3b1322D6";
+const indexerAddress = "0x2C7BCE69D5Ee84EF73CC9286416F68E60F9A61b3";
+const EASAddress = "0x4200000000000000000000000000000000000021";
+
+
+export { nftContracts, tokenAddress, indexerAddress, EASAddress };
